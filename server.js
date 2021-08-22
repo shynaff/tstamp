0528df3ea0cfa55572d81d33dbe82fcdf8b8347b
shynaff <75695975+shynaff@users.noreply.github.com>
2021-08-22 10:20:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // server.js
 // where your node app starts
-
 // init project
-var express = require('express');
-var app = express();
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const express = require('express'),
+      app = express(),
+      cors = require('cors'),
+      bodyParser = require('body-parser'), 
+      { json } = bodyParser.json();
+
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,32 +19,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get('/api/:dat', (req, res)=>{
-  let stamp, utc, result;
-  if(Date.parse(req.params.dat)){
-    stamp = Date.parse(req.params.dat);
-    utc =new Date(stamp).toUTCString();
-    result = {'unix':stamp, 'utc':utc};
-  }else 
-  if(/\d+/.test(req.params.dat)&&req.params.dat.length==13){
-    stamp = parseFloat(req.params.dat);
-    utc =new Date(stamp).toUTCString();
-    result = {'unix':stamp, 'utc':utc};
-  }else result = { error : "Invalid Date" };
-  res.send(result);
+app.get('/api', (req, res)=>{
+  const obj = {'unix': new Date().getTime(), 'utc': new Date().toUTCString()};
+  res.send(obj)
 });
 
-app.get("/api", function (req, res) {
-  let curTime = new Date();
-  res.send({"unix":Date.parse(curTime),"utc":curTime.toUTCString()});
-});
+app.get('/api/:date?', (req, res)=>{
+  const obj = (y) => {
+    let x;
+    if (Date.parse(y)) {
+      y = Date.parse(y);
+      x = new Date(y);
+    }else if (y.length == 13) {
+      x = new Date(parseFloat(y));
+    };
+    return x?{'unix': x.getTime(), 'utc': x.toUTCString()}:{ error : "Invalid Date" };
+  };
 
+  res.send(obj(req.params.date));
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
